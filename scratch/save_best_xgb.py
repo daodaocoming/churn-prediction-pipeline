1d8ecf15e69b68625ac619a26660da1fb86c7750
Doris Cai <cwz08250227@outlook.com>
2025-08-05 21:57:55 -0700

    add new save_best_xgb

--- scratch/save_best_xgb.py
+++ scratch/save_best_xgb.py
@@ -6,7 +6,8 @@ ROOT = Path.cwd().parents[0] / "churn-prediction-pipeline"
 X = joblib.load(ROOT/"data/processed/X_train.pkl")
 y = joblib.load(ROOT/"data/processed/y_train.pkl")
 
-study = optuna.load_study(study_name="xgb_pr_auc", storage="sqlite:///optuna.db")  
+study = optuna.load_study(study_name="xgb_pr_auc", storage="sqlite:///optuna.db")   
+
 best_params = study.best_trial.params
 best_params.update({"objective": "binary:logistic", "eval_metric": "aucpr", "random_state": 42, "n_jobs": -1})
 
